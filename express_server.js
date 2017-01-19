47a291ebe35280a729d43ce3e7450c8088b2f6d6
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-01-19 00:36:25 +0000

    body-parser and urls_new takes a new longURL

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,14 +11,28 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+}
+
 app.get("/urls", (req, res) => {
   res.render("urls_index", { urls: urlDatabase });
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:id", (req, res) => {
   res.render("urls_show", { shortURL: req.params.id,
                             longURL: urlDatabase[req.params.id]});
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
