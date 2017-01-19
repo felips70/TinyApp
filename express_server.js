927e6b60f2f96c831d2bb8e2f54d326a1238846f
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-01-19 22:34:25 +0000

    Added login and logout functionalities with cookies

--- express_server.js
+++ express_server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
 
 app.set("view engine", "ejs");
 
@@ -24,23 +27,44 @@ function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
 }
 
+app.get("/", (req, res) => {
+  res.redirect("/urls")
+});
+
+
 app.get("/urls", (req, res) => {
-  res.render("urls_index", { urls: urlDatabase });
+  let templateVars = { urls: urlDatabase,
+                       username: req.cookies["username"] };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { urls: urlDatabase,
+                       username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 
 app.get("/u/:shortURL", (req, res) => {
 
   if (urlDatabase.hasOwnProperty(req.params.shortURL)) {  //if user imputs nonexistent tinyURL,
-   let longURL = urlDatabase[req.params.shortURL];        //redirects to the same webpage he is in (home)
-  res.redirect(longURL);
+    let longURL = urlDatabase[req.params.shortURL];        //redirects to the same webpage he is in (home)
+    res.redirect(longURL);
   } else {
     res.redirect("/urls")
   }
+});
+
+//Logs person out
+app.post("/logout", (req, res) => {
+  res.cookie("username", '', {expires: new Date(0)});
+  res.redirect("/");
+});
+
+//Acceps a login post
+app.post("/login", (req, res) => {
+  res.cookie("username" , req.body.username);
+  res.redirect("/");
 });
 
 //Deletes selected tinyURL along with its corresponding URL
@@ -51,8 +75,8 @@ app.post("/urls/:id/delete", (req, res) => {
 
 //Updates/changes URL to which tinyURL points to
 app.post("/urls/:tinyURL", (req, res) => {
-    urlDatabase[req.params.tinyURL] = req.body.newURL;
-    res.redirect("/urls");
+  urlDatabase[req.params.tinyURL] = req.body.newURL;
+  res.redirect("/urls");
 });
 
 
@@ -63,17 +87,19 @@ app.post("/urls", (req, res) => {
   if (exitstingShortURL) {
     res.redirect(`/urls/${exitstingShortURL}`)
   } else {
-
-  let newShortURL = generateRandomString();
-  urlDatabase[newShortURL] = req.body.longURL;
-  res.redirect(`/urls/${newShortURL}`)
+    let newShortURL = generateRandomString();
+    urlDatabase[newShortURL] = req.body.longURL;
+    res.redirect(`/urls/${newShortURL}`);
   }
 });
 
 // Takes user to URL updating page
 app.get("/urls/:id", (req, res) => {
-  res.render("urls_show", { shortURL: req.params.id,
-                            longURL: urlDatabase[req.params.id]});
+  let templateVars = { shortURL: req.params.id,
+                       longURL: urlDatabase[req.params.id],
+                       username: req.cookies["username"]}
+
+  res.render("urls_show", templateVars);
 });
 
 
