b73061a44c39dd75eebeb9a95fbc0e6d73376a6b
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-01-20 08:08:36 +0000

    user specific features

--- express_server.js
+++ express_server.js
@@ -1,23 +1,22 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080; // default port 8080
+const PORT = process.env.PORT || 8080; //default port 8080
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 app.set("view engine", "ejs");
 
-let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+let urlDatabase = {}; //All URL links made by all users
 
-let usersDatabase = {};
+let usersDatabase = {}; //UserID, email, password and user-specific specific links. Defined in user registration app.post
 
 //Checks wether given URL is already in urlDatabase
-function resolveLongURL (longURL) {
-  for (let tinyURL in urlDatabase) {
-    if (urlDatabase[tinyURL] === longURL) {
+function resolveLongURL (longURL, userID) {
+  let userLinks = usersDatabase[userID].links;
+
+  for (let tinyURL in userLinks) {
+    if (userLinks[tinyURL] === longURL) {
       return tinyURL;
     }
   }
@@ -53,7 +52,9 @@ app.get("/", (req, res) => {
 //Main page where all links are displayed
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase,
-                       user_id: req.cookies["user_id"] };
+                       user_id: req.cookies["user_id"],
+                       allUsers: usersDatabase };
+  console.log(templateVars);
   res.render("urls_index", templateVars);
 });
 
@@ -61,15 +62,16 @@ app.get("/urls", (req, res) => {
 //Page where user can add a new URL
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: urlDatabase,
-                       user_id: req.cookies["user_id"] };
+                       user_id: req.cookies["user_id"],
+                       allUsers: usersDatabase };
   res.render("urls_new", templateVars);
 });
 
-
+//Redirects all tinyURLS to their corresponding longURL links
 app.get("/u/:shortURL", (req, res) => {
 
+  let longURL = urlDatabase[req.params.shortURL];        //redirects to the same webpage he is in (home)
   if (urlDatabase.hasOwnProperty(req.params.shortURL)) {  //if user imputs nonexistent tinyURL,
-    let longURL = urlDatabase[req.params.shortURL];        //redirects to the same webpage he is in (home)
     res.redirect(longURL);
   } else {
     res.redirect("/urls")
@@ -81,6 +83,7 @@ app.get("/register", (req, res) => {
   res.render("users_register");
 });
 
+//Login page
 app.get("/login", (req, res) => {
   res.render("user_login");
 });
@@ -91,23 +94,24 @@ app.post("/register", (req, res) => {
 
   if (existingEmail) {
     res.status(400).send('This email already exists');
-
-  } if (!req.body.email) {
+  } else if (!req.body.email) {
     res.status(400).send('Please type in an email');
-
   } else {
     //res.cookie("user_id" , req.body.user_id);
     let userID = generateRandomString();
     usersDatabase[userID] = {id: userID,
                              email: req.body.email,
-                             password: req.body.password };
+                             password: req.body.password,
+                             links: {} };   //Object where user-specific links are stored
+
+    res.cookie("user_id" , userID);
     res.redirect("/");
   }
 });
 
 //Logs person out
 app.post("/logout", (req, res) => {
-  res.cookie("user_id", '', {expires: new Date(0)});
+  res.clearCookie("user_id");
   res.redirect("/");
 });
 
@@ -128,13 +132,17 @@ app.post("/login", (req, res) => {
 
 //Deletes selected tinyURL along with its corresponding URL
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
+  let userIdCookie = req.cookies["user_id"];
+  delete urlDatabase[req.params.id];                        //Deletes link in urlDatabase (where all URLs are, but are not bound to the users)
+  delete usersDatabase[userIdCookie].links[req.params.id];  //Deletes link in usersDatabase (where user-specific links are)
   res.redirect("/urls");
 });
 
 //Updates/changes URL to which tinyURL points to
 app.post("/urls/:tinyURL", (req, res) => {
-  urlDatabase[req.params.tinyURL] = req.body.newURL;
+    let userIdCookie = req.cookies["user_id"];
+  urlDatabase[req.params.tinyURL] = req.body.newURL;                        //Updates link in urlDatabase (where all URLs are, but are not bound to the users)
+  usersDatabase[userIdCookie].links[req.params.tinyURL] = req.body.newURL;  //Updates link in usersDatabase (where user-specific links are)
   res.redirect("/urls");
 });
 
@@ -142,12 +150,14 @@ app.post("/urls/:tinyURL", (req, res) => {
 //Adds a new link along with new tinyURL
 app.post("/urls", (req, res) => {
 
-  let exitstingShortURL = resolveLongURL(req.body.longURL)
+  let currentUserCookie = req.cookies.user_id;
+  let exitstingShortURL = resolveLongURL(req.body.longURL, currentUserCookie);
   if (exitstingShortURL) {
     res.redirect(`/urls/${exitstingShortURL}`)
   } else {
     let newShortURL = generateRandomString();
     urlDatabase[newShortURL] = req.body.longURL;
+    usersDatabase[currentUserCookie].links[newShortURL] = req.body.longURL;
     res.redirect(`/urls/${newShortURL}`);
   }
 });
@@ -156,7 +166,8 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                        longURL: urlDatabase[req.params.id],
-                       user_id: req.cookies["user_id"]}
+                       user_id: req.cookies["user_id"],
+                       allUsers: usersDatabase}
 
   res.render("urls_show", templateVars);
 });
