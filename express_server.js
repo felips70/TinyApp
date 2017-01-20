5de509386af71ffe30495618823eea2d960070cd
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-01-20 02:56:29 +0000

    user can register, can log in, correct info in header, cannot log in with incorrect password, can log out

--- express_server.js
+++ express_server.js
@@ -5,8 +5,6 @@ const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-
-
 app.set("view engine", "ejs");
 
 let urlDatabase = {
@@ -14,6 +12,9 @@ let urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+let usersDatabase = {};
+
+//Checks wether given URL is already in urlDatabase
 function resolveLongURL (longURL) {
   for (let tinyURL in urlDatabase) {
     if (urlDatabase[tinyURL] === longURL) {
@@ -22,25 +23,45 @@ function resolveLongURL (longURL) {
   }
 }
 
+//Checks wether given email is already in usersDatabase
+function resolveEmail (email) {
+  for (let userIDs in usersDatabase) {
+    if (usersDatabase[userIDs].email === email) {
+      return email;
+    }
+  }
+}
+
+//If given email and password, if exist in usersDatabase returns corresponding user_id
+function confirmLogIn (email, password) {
+  for (let userIDs in usersDatabase) {
+    if (email === usersDatabase[userIDs].email && password === usersDatabase[userIDs].password) {
+      return usersDatabase[userIDs].id;
+    }
+  }
+}
 
 function generateRandomString() {
   return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
 }
 
+//Root, redirects to /urls
 app.get("/", (req, res) => {
   res.redirect("/urls")
 });
 
-
+//Main page where all links are displayed
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase,
-                       username: req.cookies["username"] };
+                       user_id: req.cookies["user_id"] };
   res.render("urls_index", templateVars);
 });
 
+
+//Page where user can add a new URL
 app.get("/urls/new", (req, res) => {
   let templateVars = { urls: urlDatabase,
-                       username: req.cookies["username"] };
+                       user_id: req.cookies["user_id"] };
   res.render("urls_new", templateVars);
 });
 
@@ -55,16 +76,54 @@ app.get("/u/:shortURL", (req, res) => {
   }
 });
 
+//Registration page
+app.get("/register", (req, res) => {
+  res.render("users_register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("user_login");
+});
+
+//Registers new email and password to the local object database
+app.post("/register", (req, res) => {
+  let existingEmail = resolveEmail(req.body.email);
+
+  if (existingEmail) {
+    res.status(400).send('This email already exists');
+
+  } if (!req.body.email) {
+    res.status(400).send('Please type in an email');
+
+  } else {
+    //res.cookie("user_id" , req.body.user_id);
+    let userID = generateRandomString();
+    usersDatabase[userID] = {id: userID,
+                             email: req.body.email,
+                             password: req.body.password };
+    res.redirect("/");
+  }
+});
+
 //Logs person out
 app.post("/logout", (req, res) => {
-  res.cookie("username", '', {expires: new Date(0)});
+  res.cookie("user_id", '', {expires: new Date(0)});
   res.redirect("/");
 });
 
-//Acceps a login post
+//Accepts a login post
 app.post("/login", (req, res) => {
-  res.cookie("username" , req.body.username);
-  res.redirect("/");
+  let tryEmail = req.body.email;
+  let tryPassword = req.body.password;
+  let userIdCookie = confirmLogIn(tryEmail, tryPassword);
+
+  if (!userIdCookie) {
+    res.status(403).send('Wrong email or password');
+  } else {
+    res.cookie("user_id" , userIdCookie);
+    res.redirect("/");
+  }
+
 });
 
 //Deletes selected tinyURL along with its corresponding URL
@@ -97,7 +156,7 @@ app.post("/urls", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id,
                        longURL: urlDatabase[req.params.id],
-                       username: req.cookies["username"]}
+                       user_id: req.cookies["user_id"]}
 
   res.render("urls_show", templateVars);
 });
@@ -106,4 +165,3 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
